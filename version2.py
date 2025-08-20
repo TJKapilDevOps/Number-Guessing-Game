e4fa883f33d7115c7a3274c09ef734317549994f
TJKapilDevOps <toml29686@gmail.com>
2025-08-20 16:30:00 -0700

    Update version2.py

--- version2.py
+++ version2.py
@@ -1,1 +1,46 @@
+from random import randint
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+# Function to check users guess against actual answer.
+def check_answer(user_guess, actual_answer, turns):
+    """Checks answer against guess and returns the number of turns remaining."""
+    if user_guess > actual_answer:
+        print("Too high.")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {actual_answer}")
+
+# Function to set difficulty
+
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").strip().lower()
+    if level == "easy":
+        return  EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"Psst, the correct answer is {answer}")
+    # Function to set difficulty
+    turns = set_difficulty()
+    # Let the user guess a number
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print(f"You've run out of guesses. Refresh the page to run again.")
+            return
+        elif guess != answer:
+            print("Guess again.")
+
+game()
